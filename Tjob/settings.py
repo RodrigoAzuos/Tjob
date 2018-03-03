65daf61fa6b9cf664d36082a98fbf87c7392c67d
RodrigoAzuos <rodrill.068@gmail.com>
2018-03-03 11:19:00 -0300

    recolocado a secret_key

--- Tjob/settings.py
+++ Tjob/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '9@gp5arfs82x5sj!p0k1@0*@v!6q^nco560_wj)vt%5v&qgf%0'
+SECRET_KEY = '9@gp5arfs82x5sj!p0k1@0*@v!6q^nco560_wj)vt%5v&qgf%0'
 #
 # # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
+# SECRET_KEY = config('SECRET_KEY')
+# DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = []
 
 
